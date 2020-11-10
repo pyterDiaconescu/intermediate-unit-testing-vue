2a41b11a45990688e2e04d8e4971630e3f75b55e
petrero <petrero@gmail.com>
2020-11-10 11:59:47 +0200

    3.2.Testing Props & User Interaction
    Checking the default random number
    Looking at the component, we know that the default value for randomNumber is 0, so why would you even test for this? Well, what if someone else on our team changed the default randomNumber? Testing it gives us confidence 0 will always be displayed when the component is first loaded.
    Our first step to test this is to mount the component we are testing (RandomNumber.vue), which gives us the wrapper that lets us dive into the component and test what we need to.
    Here, we’re using the wrapper to get the html of our RandomNumber component, and asserting that we expect that html toContain a span with a 0 in its inner HTML.
    If we run this test by typing npm run test:unit in our terminal, we’ll see that now only 2 tests are failing, and we’ve gotten our first test to pass. Now we can move on to our next test, which requires some user interaction.

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -3,7 +3,8 @@ import { mount } from '@vue/test-utils'
 
 describe('RandomNumber', () => {
   test('By default, randomNumber data value should be 0', () => {
-    expect(true).toBe(false)
+    const wrapper = mount(RandomNumber)
+    expect(wrapper.html()).toContain('<span>0</span>')
   })
   test('If button is clicked, randomNumber data value should be between 1 an nd 10', () => {
     expect(true).toBe(false)
