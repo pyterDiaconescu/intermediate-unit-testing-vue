4cbdda29291215baee8c59463f51b9d2fcdde844
petrero <petrero@gmail.com>
2020-11-11 08:49:04 +0200

    5.4.Testing API Calls
    Once we’ve installed the library with npm i flush-promises --save-dev, we’ll import it into our testing file and await the flushing of the promises.
    Now that we’ve ensured promises will be resolved before our assertions are run, we can write those assertions.
    
    Our Assertions
    First up, we’ll want to make sure our API call is only happening once.
    We’re simply running the method .toHaveBeenCalledTimes() and passing in the number of times we expect getMessage to have been called: 1. Now we’ve ensured that we aren’t accidentally hitting our server more times than we should be.
    Next up, we need to check that our component is displaying the message it received from our getMessage request. In the MessageDisplay component’s template, the p tag that displays the message has an id to be used for tests: data-testid="message"
    We learned about these testing ids in the previous lesson. We’ll use that id to find the element, then assert that its text content should be equal to the value our mocked getMessage request resolved with: mockMessage
    If we run npm run test:unit in the terminal, we’ll see our newly written test is passing! We can now move on to our second test, where we’ll mock a failed getMessage request and check that our component is displaying the error.

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -1,17 +1,23 @@
 import MessageDisplay from '@/components/MessageDisplay'
 import { mount } from '@vue/test-utils'
 import { getMessage } from '@/services/axios'
+import flushPromises from 'flush-promises'
 
 jest.mock('@/services/axios')
 
 describe('MessageDisplay', () => {
   it('Calls getMessage and displays message', async () => {
     const mockMessage = "Hello from the db!"
+    // mock the API call
     getMessage.mockResolvedValueOnce({ text: mockMessage })
     const wrapper = mount(MessageDisplay)
     // wait for promise to resolve
+    await flushPromises()
     // check that call happened once
+    expect(getMessage).toHaveBeenCalledTimes(1)
     // check that component displays message
+    const message = wrapper.find('[data-testid="message"]').element.textContent
+    expect(message).toEqual(mockMessage)
   })
 
   it('Displays an error when getMessage call fails', async () => {
